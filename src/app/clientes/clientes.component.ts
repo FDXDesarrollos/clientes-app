47b5f699510721825d4b1c6e8b568529539cdb37
fjuarez <fdxdesarrollos@gmail.com>
2023-08-07 19:35:29 -0600

    Se concluyo el CRUD de clientes

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -18,6 +19,34 @@ export class ClientesComponent implements OnInit {
     this.clienteService.getClientes().subscribe((clientes) => {
       this.clientes = clientes;
     });
+  }
+
+  delete(cliente: Cliente): void {
+    Swal.fire({
+      title: 'Estas seguro?',
+      text: `¿Eliminar al cliente: "${cliente.nombre} ${cliente.apellido}"?`,
+      icon: 'warning',
+      showCancelButton: true,
+      /*confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',*/
+      confirmButtonText: 'Si, eliminar',
+      cancelButtonText: 'No, cancelar',      
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+
+        this.clienteService.delete(cliente.id)
+        .subscribe( (response) => {
+          this.clientes = this.clientes.filter(cli => cli !== cliente)
+          Swal.fire(
+            'Cliente Eliminado!',
+            `Cliente "${cliente.nombre}" eliminado con éxito`,
+            'success'
+          )
+        });
+      }
+    })    
+
   }
 
 }
